6fe0c764d1225bff464e5a0265a96a15f55df66f
Jairo <jgtrue@protonmail.com>
2023-03-30 20:25:09 +0200

    route update

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/login', function () {
     return view('login');
 })->name('login');
 
-Route::get('/template', function () {
+Route::get('/', function () {
     return view('/templates/template');
 })->name('template');
