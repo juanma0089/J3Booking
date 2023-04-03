915b1f3f90d18b2895203b3c2edb1b5ee87f130c
Jonatan Garcia <jonatangarciabarrera@gmail.com>
2023-04-03 17:28:07 +0200

    usertable in progress

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -6,15 +6,15 @@
 
 @section('body')
     <section class="vh-100 ">
-        <div class="container overflow-hidden">
-            <table class="table align-middle mb-0 mt-5 bg-white">
-                <thead class="bg-light">
+        <div class="container">
+            <table class="table align-middle mb-0 mt-5 bg-custom text-white">
+                <thead class="bg-custom">
                     <tr>
-                        <th>Nombre</th>
-                        <th>Puesto</th>
-                        <th>Rol</th>
-                        <th>Teléfono</th>
-                        <th>Acciones</th>
+                        <th class="name-head-usertable">Nombre</th>
+                        <th class="jobtitle-head-usertable">Puesto</th>
+                        <th class="role-head-usertable">Rol</th>
+                        <th class="phone-head-usertable">Teléfono</th>
+                        <th class="actions-head-usertable">Acciones</th>
                     </tr>
                 </thead>
                 <tbody>
